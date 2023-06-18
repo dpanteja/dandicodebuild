ae8d0eaf99b140a653e8606945355aeff095c68e
dpanteja <dparasa@hotmail.com>
2023-06-18 19:42:33 +0530

    Initial Commit

---
+++ src/main/java/dandi/latest/dandicodebuild/DandiCodebuildApplication.java
@@ -0,0 +1,13 @@
+package dandi.latest.dandicodebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DandiCodebuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DandiCodebuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dandi/latest/dandicodebuild/DandiCodebuildApplicationTests.java
@@ -0,0 +1,13 @@
+package dandi.latest.dandicodebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DandiCodebuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
